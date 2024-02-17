270bb9152c1d566cadbd33d5d407f673e54724f9
joycenjoro1 <njorogejoyce60@gmail.com>
2024-02-16 23:38:38 -0800

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-
-if (arg1 !== undefined && arg2 !== undefined) {
-    console.log(`${arg1} is ${arg2}`);
-} else {
-    console.log("Usage: node script.js <argument1> <argument2>");
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
