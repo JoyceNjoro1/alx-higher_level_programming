e33d586b8a14467bb6bec9e572c69bb2be7c74bc
Joyce Njoroge <136052595+JoyceNjoro1@users.noreply.github.com>
2024-04-08 16:46:54 +0300

    all

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-# Take the URL as an argument
-url=$1
-
-# Send a GET request with the specified header using curl
-response=$(curl -s -H "X-School-User-Id: 98" "$url")
-
-# Check if the response is empty
-if [ -z "$response" ]; then
-    echo "Error: Empty response received"
-    exit 1
-fi
-
-# Display the response
-echo "$response"
-
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
