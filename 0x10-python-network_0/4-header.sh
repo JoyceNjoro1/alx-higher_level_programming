1b7aeac826dcee5315f06e2579d3ae3ee896aa8a
Joyce Njoroge <136052595+JoyceNjoro1@users.noreply.github.com>
2024-04-08 10:38:38 +0300

    all

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # Send a GET request to a given URL with a header variable.
 curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
