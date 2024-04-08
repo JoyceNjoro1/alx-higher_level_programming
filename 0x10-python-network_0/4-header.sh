6417677aa80f436db044412870a9df9259dbd3e3
Joyce Njoroge <136052595+JoyceNjoro1@users.noreply.github.com>
2024-04-08 14:54:12 +0300

    all

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+
+# Check if the URL argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# URL provided as argument
+URL=$1
+
+# Send GET request with custom header and display response body
+curl -sSL -H "X-School-User-Id: 98" "$URL"
+
